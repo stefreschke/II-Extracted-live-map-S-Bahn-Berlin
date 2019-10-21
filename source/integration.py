0a200c4ee3f243a584f12109cedf862160e80683
Stefan Reschke <stefan.reschke@outlook.de>
2019-10-21 18:06:06 +0200

    Some fancy dataframe wrangling

--- source/integration.py
+++ source/integration.py
@@ -3,6 +3,7 @@ Integration step for S Bahn Berlin data. Run this file to load and convert S Bah
 files accordingly.
 """
 import json
+import pandas as pd
 from pandas.io.json import json_normalize
 
 
@@ -12,9 +13,25 @@ def main():
     :return:
     """
     with open('../data/traffic_data.json') as file:
-        lines = file.readlines()
-        data = [json.loads(object) for object in lines]
-        frame = json_normalize(data)
+        for index, line in enumerate(file):
+            parts = line.split("|")
+            timestamp = parts[0]
+            suspected_json = "|".join(parts[1:])
+            data_set = json.loads(suspected_json)
+            entries = data_set["t"]
+            df = json_normalize(entries)
+            df.drop(columns=["p"], inplace=True)
+            df.x = pd.to_numeric(df["x"])
+            df.y = pd.to_numeric(df["y"])
+            df[["x", "y"]] = df[["x", "y"]] / 10e5
+            df["n"] = df["n"].astype("category")
+            df[["c", "d"]] = df[["c", "d"]].astype("int8")
+            df["rd"] = df["rd"].astype("category")
+
+
+class JSONParseException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 if __name__ == '__main__':
