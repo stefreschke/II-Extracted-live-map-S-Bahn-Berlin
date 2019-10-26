4dc8c2a23c6f76901b0ca665b6f085b44205a170
Your Name <you@example.com>
2019-10-26 18:57:00 +0200

    ensure log folder gets created

--- source/log_stuff.py
+++ source/log_stuff.py
@@ -1,5 +1,5 @@
 import logging
-
+import os
 
 def init_logger():
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
@@ -10,9 +10,10 @@ def init_logger():
 def debug_logger(formatter):
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
+    os.makedirs("../logs/", exist_ok=True)
     file_handler = logging.FileHandler(filename="../logs/log.log")
     file_handler.setFormatter(formatter)
     logger = logging.getLogger('extraction')
     logger.setLevel(logging.INFO)
-    logger.addHandler(file_handler)
+    #logger.addHandler(file_handler)
     logger.addHandler(console_handler)
