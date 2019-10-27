4a3a40af9624410c1f2f545990770477025f7f9d
Stefan Reschke <stefan.reschke@outlook.de>
2019-10-27 12:46:55 +0100

    Fix operationalization

--- source/extract.py
+++ source/extract.py
@@ -7,12 +7,20 @@ import sqlite3
 import pandas as pd
 
 import res
+from log_stuff import init_logger
 
-SQLITE_FILENAME = '../data/' + "s-bahn-livekarte.db"
+logger = logging.getLogger('extraction')
 
 
-def main():
-    with sqlite3.connect(res.SQLITE_FILE) as conn:
+def main(sqlite_file=res.SQLITE_FILE, data_file=res.DATA_FILE, stops_file=res.STOPS_FILE):
+    """
+    Perform extraction for given files (which all default to values from res.py).
+    :param sqlite_file: Database to write stuff to.
+    :param data_file: json-file to get all stuff from.
+    :param stops_file: json-file containing data on stops.
+    :return:
+    """
+    with sqlite3.connect(sqlite_file) as conn:
         conn.executescript("""
 CREATE TABLE "snapshot" (
     "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -45,14 +53,14 @@ CREATE TABLE "projection" (
         cursor = conn.cursor()
 
         # big snapshot json file
-        with open(res.DATA_FILE, encoding='utf-8') as file:
+        with open(data_file, encoding='utf-8') as file:
             for index, line in enumerate(file):
                 parts = line.split("|")
                 suspected_json = "|".join(parts[1:])
                 try:
                     data_set = json.loads(suspected_json)
                 except json.decoder.JSONDecodeError:
-                    print("Invalid line at {}".format(index))
+                    logger.warning("Invalid line at %d", index)
                     continue
                 at_time = parts[0].split(" ")[0] + " " + data_set["ts"]
                 cursor.execute('INSERT INTO snapshot (taken_at) VALUES (?)', (at_time,))
@@ -72,18 +80,19 @@ CREATE TABLE "projection" (
                             'INSERT INTO projection(datarecord_id, x, y, t, d) VALUES (?, ?, ?, '
                             '?, ?)',
                             (datarecord_id, p["x"], p["y"], p["t"], p["d"] if "d" in p else None))
-                if index > 3000:
-                    break  # 1GB Limit
+                if index > 3600:
+                    break  # 1GB Limit by try out
 
         # stops
-        with open(res.STOPS_FILE, encoding='utf-8') as file:
+        with open(stops_file, encoding='utf-8') as file:
             stops = json.loads(file.read())
             df = pd.DataFrame(stops["stops"])
             df = df.rename(columns=df.iloc[0]).drop(df.index[0])[["x", "y", "name"]]
             df.to_sql(name="stops", con=conn, index_label="id")
 
-    print("Finished")
+    logger.info("Finished reading data from %s, wrote to %s", data_file, sqlite_file)
 
 
 if __name__ == '__main__':
+    init_logger()
     main()
